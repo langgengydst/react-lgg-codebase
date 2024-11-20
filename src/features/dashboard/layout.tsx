f4177325c3edad22871c5554e7d8ed06319b9eec
telkomdev-amudia <amudia.ktaruna@gmail.com>
2024-11-20 19:53:07 +0700

    test: init

--- src/features/dashboard/layout.tsx
+++ src/features/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 export default function DashboardLayout() {
   return (
     <div>
-      <h1>Layout Dashboard</h1>
+      <h1>Layout Dashboard </h1>
       <Outlet />
     </div>
   );
